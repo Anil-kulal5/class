54f03134e715cca6aef3863d29a52eb9f0c4d97e
Anil-Kulal5 <kulalanil597@gmail.com>
2023-06-26 14:05:48 +0530

    this is my first upload

---
+++ iterable.py
@@ -0,0 +1,33 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:/Users/kk/Desktop/Reusable.py ==============
+1800 <class 'int'>
+>>> 
+============== RESTART: C:/Users/kk/Desktop/Reusable.py ==============
+(10, 10, 18) <class 'tuple'>
+>>> 
+============== RESTART: C:/Users/kk/Desktop/Reusable.py ==============
+(10, 10, 18) <class 'tuple'>
+[1, 2, 3, 4, 5, 6] <class 'list'>
+>>> 
+============== RESTART: C:/Users/kk/Desktop/Reusable.py ==============
+(10, 10, 18) <class 'tuple'>
+['123456'] <class 'list'>
+>>> 
+============== RESTART: C:/Users/kk/Desktop/Reusable.py ==============
+(10, 10, 18) <class 'tuple'>
+['123456'] <class 'list'>
+{12} <class 'set'>
+>>> 
+============== RESTART: C:/Users/kk/Desktop/Reusable.py ==============
+(10, 10, 18) <class 'tuple'>
+['123456'] <class 'list'>
+{12} <class 'set'>
+{1: '12', 2: '12'} <class 'dict'>
+>>> 
+============== RESTART: C:/Users/kk/Desktop/Reusable.py ==============
+(10, 10, 18) <class 'tuple'>
+['123456'] <class 'list'>
+{12} <class 'set'>
+{1: '12', 2: '12'} <class 'dict'>
---
+++ nexted.py
@@ -0,0 +1,16 @@
+i=1
+while i<25:
+    print(i)
+    i+=1
+else:
+    print('i am else statement')
+    print('i am  block statement')
+#while and else statement
+
+    i=50
+    while i<28:
+        print(i)
+        i+=1
+    else:
+        print('i am else statement')
+        print('i am block statement')
